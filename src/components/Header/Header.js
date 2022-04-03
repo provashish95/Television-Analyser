de9b0022a66709f9c6b0d12edd85bc121d700513
Provashish Roy <provashishroy95@gmail.com>
2022-04-04 00:26:15 +0600

    added custome navlink

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light  bg-navbar-color">
                 <div className="container-fluid">
 
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                        <span className="navbar-toggler-icon icon-style"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav m-auto">
-                            <li className="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
+                        <ul className="navbar-nav m-auto ">
+                            <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")} to='/'>Home</NavLink>
+                            <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")} to='/reviews'>Reviews</NavLink>
+                            <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")} to='/dashboard'>Dashboard</NavLink>
+                            <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")} to='/blogs'>Blogs</NavLink>
+                            <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")} to='/about'>About</NavLink>
                         </ul>
                     </div>
                 </div>
