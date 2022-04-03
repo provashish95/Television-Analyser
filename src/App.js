95e3ed227434fdbfaafc7d79ce39cf41f882a9c4
Provashish Roy <provashishroy95@gmail.com>
2022-04-03 23:21:53 +0600

    install react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>this is television app</h1>
+      <Routes>
+        <Route path='/home' element={<h2>my name is roy</h2>}></Route>
+      </Routes>
     </div>
   );
 }
