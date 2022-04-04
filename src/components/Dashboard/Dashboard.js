b5ce27a81574afd353d44911153b16ae78cff825
Provashish Roy <provashishroy95@gmail.com>
2022-04-04 16:35:49 +0600

    load charts data and add line charts on dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 import './Dashboard.css';
 
 const Dashboard = () => {
+    const [chartsData, setChartsData] = useState([]);
+    useEffect(() => {
+        fetch("data.json")
+            .then(res => res.json())
+            .then(data => setChartsData(data))
+    }, []);
+
+    console.log(chartsData);
     return (
-        <div>
-            <h4>this is dashboard page</h4>
-        </div>
+        <section className='container'>
+            <div className="row mt-5">
+                <div className="col-sm-12 col-md-6 col-lg-6">
+                    <LineChart width={500} height={300} data={chartsData}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <Tooltip />
+
+                        <Line type="monotone" dataKey="revenue" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    </LineChart>
+                    <h6 className='text-center'>This is Line chart</h6>
+                </div>
+                <div className="col-sm-12 col-md-6 col-lg-6">
+
+
+
+                </div>
+            </div>
+        </section>
     );
 };
 
---
+++ src/components/ReChart/ReChart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ReChart.css';
+
+const ReChart = (props) => {
+    console.log(props);
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ReChart;
