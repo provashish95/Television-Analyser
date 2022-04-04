6a7156bcd31538511b0fdbacb8d736ed18d4e048
Provashish Roy <provashishroy95@gmail.com>
2022-04-05 01:14:37 +0600

    updated not found page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './NotFound.css';
+import Img from '../../assets/image/notFound.png';
+
 
 const NotFound = () => {
-    return (
-        <div>
-            <h1 style={{ color: 'red' }}>Page not found 404!</h1>
-        </div>
+    return (~~
+        <div className= 'text-center my-5' >
+            <img src={Img} alt="notFoundImg" className='errorImg' />
+            <h1 className='text-danger fw-bolder '>404</h1>
+            <h3 className='text-muted mb-4'>Sorry, The Page Not Found</h3>
+            <p className='text-muted fw-bold mb-5'>The page you requested could not be found !</p>
+        </div >
     );
 };
 
