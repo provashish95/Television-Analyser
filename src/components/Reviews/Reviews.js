3269a8fbcc8114fb1f38f7d800fede226a3d798d
Provashish Roy <provashishroy95@gmail.com>
2022-04-05 18:30:43 +0600

    updated app.js and reviews file

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -4,11 +4,11 @@ import Review from '../Review/Review';
 import '../Home/Home.css';
 
 const Reviews = () => {
-    const [reviews, setReviews] = useReviews();
+    const [reviews] = useReviews();
 
     return (
         <div className="container">
-            <h4 className='text-center fw-bold title-style mt-5'>Customer reviews</h4>
+            <h4 className='text-center fw-bold title-style mt-5'>What our customers say!</h4>
             <div className="row">
                 {
                     reviews.map(review => <Review key={review.id} review={review}></Review>)
